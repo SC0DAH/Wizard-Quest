5d47c647b8e2479b4da4ad95373ecc2efc1dd98f
Senne Cockx <ditissenne@gmail.com>
2025-02-05 19:16:57 +0100

    Add project files.

---
+++ WizardQuest/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace WizardQuest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Wizard! What do you want to name yourself?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Choose a spell to cast during battle!");
+            string spell = ChooseSpell();
+            Wizard user01 = new Wizard(name, spell);
+            Console.WriteLine($"Welcome {user01.Name}! You're set to go on adventure, your spell is {user01.Spell}!");
+            Console.WriteLine($"Press enter to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static string ChooseSpell()
+        {
+            Console.WriteLine($"1. {Spells.Fireball}\n2. {Spells.Waterwave}\n3. {Spells.Earthquake}\n4. {Spells.Poison}");
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Ongeldige keuze, probeer opnieuw:");
+            }
+            string spellChoice = "";
+            switch ((Spells)choice - 1)
+            {
+                case Spells.Fireball:
+                    spellChoice = Spells.Fireball.ToString();
+                    break;
+                case Spells.Waterwave:
+                    spellChoice = Spells.Waterwave.ToString();
+                    break;
+                case Spells.Earthquake:
+                    spellChoice = Spells.Earthquake.ToString();
+                    break;
+                case Spells.Poison:
+                    spellChoice = Spells.Poison.ToString();
+                    break;
+                default:
+                    break;
+            }
+            return spellChoice;
+        }
+    }
+}
---
+++ WizardQuest/Wizard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardQuest
+{
+    internal class Wizard
+    {
+        public string Name;
+        public string Spell;
+
+        public Wizard(string wizardName, string spellName)
+        {
+            Name = wizardName;
+            Spell = spellName;
+        }
+    }
+}
