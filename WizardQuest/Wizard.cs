cd0a4d1529db8a82d42d426c761608a8d2b651b3
SenneCockx06 <s160854@ap.be>
2025-02-07 18:17:03 +0100

    Fixed bug where user would clear stage by healing

--- WizardQuest/Wizard.cs
+++ WizardQuest/Wizard.cs
@@ -10,13 +10,16 @@ namespace WizardQuest
     internal class Wizard
     {
         static Entity entity;
+        bool isEntityDead = false;
+
         public string Name;
         public string Spell;
-        int CombatPower = 2;
+        int CombatPower = 5;
         int HitPoints = 10;
         int maxHP = 10;
         int HPUP = 2;
-        int Stage = 1;
+        int Stage = 0;
+        bool IsUserDead = false;
         
 
         public Wizard(string wizardName, string spellName)
@@ -46,19 +49,53 @@ namespace WizardQuest
         {
             Console.WriteLine("You're running towards the castle...");
             Thread.Sleep(1500);
+            Stage++;
             entity = new Entity(Stage);
+            isEntityDead = false; // new entity spawns
             Console.WriteLine($"You've reached stage {Stage}! An entity has spawned with {entity.HP}HP and {entity.CP}CP!");
             Thread.Sleep(1500);
-            Interact();
+            bool result = StageStart();
+            if (result)
+            {
+                Console.WriteLine($"You died while on the search to the book of spells. You reached stage {Stage}");
+            }
+            else
+            {
+                Adventure();
+            }
         }
 
-        private void Interact()
+        private bool StageStart()
         {
-            Console.WriteLine($"Choose what you want to do:\n1. Fight the entity ({CombatPower} damage)\n2. Heal up ({HPUP} HP)");
+            while (true) // stage will continue until user or entity is dead
+            {
+                Console.WriteLine($"It's your turn {Name}! You have {HitPoints}HP and {CombatPower}CP");
+                UserTurn();
+                if (isEntityDead)
+                {
+                    break;
+                }
+                Console.WriteLine("It's the entity's turn now!");
+                EntityTurn();
+                if (IsUserDead)
+                {
+                    break;
+                }
+            }
+            return IsUserDead;
+        }
+
+        private void UserTurn()
+        {
+            Console.WriteLine($"Choose what you want to do:\n1. Fight the entity ({CombatPower} damage)");
+            if (HitPoints < maxHP) // only available when user is not at full health
+            {
+                Console.WriteLine($"2. Heal up ({HPUP}HP)");
+            }
             int choice;
             while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 2)
             {
-                Console.WriteLine("Bad choice, try again!");
+                    Console.WriteLine("Bad choice, try again!");    
             }
             if (choice == 1)
             {
@@ -66,22 +103,49 @@ namespace WizardQuest
             }
             else
             {
-                HitPoints += HPUP;
+                HealUp();
             }
+        }
+
+        private void EntityTurn()
+        {
+            TakeDamage();
         }
 
         private void DoDamage()
         {
-            bool isDead = entity.TakeDamage(CombatPower);
+            isEntityDead = entity.TakeDamage(CombatPower);
             Console.WriteLine($"You casted your {Spell} and did {CombatPower} damage!");
-            if (isDead)
+            if (isEntityDead)
             {
-                Console.WriteLine("This attack was brutal and killed the entity!");
+                Console.WriteLine($"This attack was brutal and killed the entity!\nCongrats on clearing stage {Stage}!");
             }
             else
             {
                 Console.WriteLine($"The entity has {entity.HP}HP remaining.");
             }
         }
+
+        private void HealUp()
+        {
+            HitPoints += HPUP; // math.min doen
+            Console.WriteLine($"You've healed up {HPUP}HP, you now have {HitPoints}HP!");
+        }
+
+        private void TakeDamage()
+        {
+            string[] attacks = { "bite", "punch", "kick" };
+            Random random = new Random();
+            int index = random.Next(0, attacks.Length);
+            HitPoints = Math.Max(0, HitPoints - entity.CP);
+            Console.WriteLine($"The entity decided to {attacks[index]} you, dealing {entity.CP} damage!");
+            if (HitPoints == 0)
+            {
+                Console.WriteLine("This attack was too much for you to handle! You died :c");
+                IsUserDead = true;
+            }
+        }
+
+
     }
 }
